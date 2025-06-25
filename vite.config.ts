cd24929d68205419fe1ff24708c71af86a50c2f4
Indraneel Bose <91256715+kaun-neel@users.noreply.github.com>
2025-06-25 15:37:05 +0530

    Fix Git merge conflict in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,12 +7,9 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-<<<<<<< HEAD
   server: {
     host: true,
     port: 3001, 
     strictPort: true 
   }
-=======
->>>>>>> 117c30fa8015fd276c748e4ca9ae5645df66f8c5
 });
